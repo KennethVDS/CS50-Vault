32b9a2ffd5a1887fd90bbec4c9e51801c59bc41e
Knnth <49682872+KennethVDS@users.noreply.github.com>
2024-02-03 10:14:51 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+#Ask user for their name
+#Remove whitespace from str and capitalize user's name
+name = input ("What's your name? ").strip().title()
+
+#Say hello to user
+print (f"hello, {name}")
+# Note to self
